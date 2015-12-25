e84ef0e8da71998d0f1892f0d3b999f740f9e85e
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2015-12-25 21:48:55 +0100

    first commit

---
+++ app/src/main/java/edu/upc/fib/idi/idireceptes/model/Ingredient.java
@@ -0,0 +1,18 @@
+package edu.upc.fib.idi.idireceptes.model;
+
+/**
+ * Created by casassg on 25/12/15.
+ *
+ * @author casassg
+ */
+public class Ingredient {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/edu/upc/fib/idi/idireceptes/model/Recepta.java
@@ -0,0 +1,54 @@
+package edu.upc.fib.idi.idireceptes.model;
+
+import java.util.List;
+
+/**
+ * Created by casassg on 25/12/15.
+ *
+ * @author casassg
+ */
+public class Recepta {
+    private String description;
+    private List<Integer> ingredients;
+    private String title;
+    private int id;
+
+    public Recepta(String description, List<Integer> ingredients, String title, int id) {
+        this.description = description;
+        this.ingredients = ingredients;
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Integer> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Integer> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
